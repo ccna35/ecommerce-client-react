1ab24a6f41d3a2531276eff4f58939c64e8dc5ce
Shawky Khalil <shawkykhalil@gmail.com>
2023-07-22 13:57:32 +0300

    "Added new API url"

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080/api" });
+const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080/api" || "https://ecommerce-store-server-vsnh.onrender.com/api" });
 
 export const apiSlice = createApi({
   baseQuery,
