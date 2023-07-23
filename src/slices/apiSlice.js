30e1766dd0b4fbc44a7ef2d653f35deac458cb36
Shawky Khalil <shawkykhalil@gmail.com>
2023-07-23 11:27:10 +0300

    "Added env variables"

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,12 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080/api" || "https://ecommerce-store-server-vsnh.onrender.com/api" });
+const API_URL = import.meta.env.DEV
+  ? "http://localhost:8080/api"
+  : "https://ecommerce-store-server-vsnh.onrender.com/api";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: API_URL,
+});
 
 export const apiSlice = createApi({
   baseQuery,
